ef5d60c608a3f05fbf83d6da1a3ffa337e2670c4
Famannn <83688777+Famannn@users.noreply.github.com>
2022-05-29 22:29:08 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+
+// components
+import MyNavbar from "./components/my-navbar/mynavbar.component"
+import MyCarousal from "./components/my-carousal/my-carousal.component"
+import TitleMessage from "./components/title-message/title-message.component"
+
+const App = () => {
+  return (
+    (<div>
+      <MyNavbar />
+      <MyCarousal />
+      <TitleMessage />
+    </div>)
+  );
+}
+
+export default App;
